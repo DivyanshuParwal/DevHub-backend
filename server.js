5cb016000ab4cff1c6d8fb72f3aacef0cb0f19ea
Divyanshu Parwal <68116519+DivyanshuParwal@users.noreply.github.com>
2023-02-27 22:56:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,16 +32,6 @@ app.use((error, req, res, next) => {
 	res.json({ message: error.message || 'An unkown error occurred' });
 });
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('server is running'));
